4e54a9fcafdb78ea9ea85b53c5c1939f3cd850da
Mikola Maistrenko <uncle.claus@gmail.com>
2017-01-07 10:14:47 +0200

    First commit

---
+++ src/main/java/ua/java/fain/foo.java
@@ -0,0 +1,7 @@
+package ua.java.fain;
+
+/**
+ * Created by Maistrenko on 07.01.2017.
+ */
+public class foo {
+}
---
+++ src/main/java/ua/java/fain/test.java
@@ -0,0 +1,10 @@
+package ua.java.fain;
+
+/**
+ * Created by Maistrenko on 07.01.2017.
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
