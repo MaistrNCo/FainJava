a7a2628e7555cb99106fd89440e2cd7a1f6e1115
Mikola Maistrenko <uncle.claus@gmail.com>
2017-01-09 11:21:49 +0200

    Merged all branches

--- src/main/java/ua/java/fain/foo.java
+++ src/main/java/ua/java/fain/foo.java
@@ -7,14 +7,12 @@ public class foo {
     int weigth;
     String name;
     String color;
-
     public void saySomething(){
         System.out.println("Hello, happy New Year!!!!");
     }
     public void sayYourName(){
         System.out.println("May name is "+ name);
     }
-
     public void sayYourColor(){
         System.out.println("My color is " + color);
     }
