9f7b5411bb548aadaa879a6dbd577ea8325c4a54
Mikola Maistrenko <uncle.claus@gmail.com>
2017-01-09 20:26:43 +0200

    Added Calculator class (BorderLayout +GridLayout) used array for buttons

--- src/main/java/ua/java/fain/Calculator.java
+++ src/main/java/ua/java/fain/Calculator.java
@@ -1,7 +1,6 @@
 package ua.java.fain;
 import javax.swing.*;
 import java.awt.*;
-import java.text.Format;
 
 /**
  * Created by Maistrenko on 09.01.2017.
@@ -12,16 +11,8 @@ public class Calculator {
     JTextField displayField;
     JFormattedTextField displayFieldFormated;
 
-    JButton button0;
-    JButton button1;
-    JButton button2;
-    JButton button3;
-    JButton button4;
-    JButton button5;
-    JButton button6;
-    JButton button7;
-    JButton button8;
-    JButton button9;
+    JButton buttons[];
+
     JButton buttonPoint;
     JButton buttonEqual;
     JButton buttonPls;
@@ -42,22 +33,19 @@ public class Calculator {
         windowContent.setLayout(bl);
       //create the display field & place it in the north of the window
         displayField = new JTextField(30);
-        Format rJstf =
+
         displayFieldFormated = new JFormattedTextField();
-        //windowContent.add("North",displayField);
+        displayFieldFormated.setColumns(30);
+        displayFieldFormated.setHorizontalAlignment(JTextField.RIGHT);
+
         windowContent.add("North",displayFieldFormated);
 
       //create buttons
-        button0 = new JButton("0");
-        button1 = new JButton("1");
-        button2 = new JButton("2");
-        button3 = new JButton("3");
-        button4 = new JButton("4");
-        button5 = new JButton("5");
-        button6 = new JButton("6");
-        button7 = new JButton("7");
-        button8 = new JButton("8");
-        button9 = new JButton("9");
+        buttons = new JButton[10];
+        for (int index = 0; index < buttons.length; index++) {
+            buttons[index] = new JButton(Integer.toString(index));
+        }
+
         buttonPoint = new JButton(".");
         buttonEqual = new JButton("=");
 
@@ -69,16 +57,11 @@ public class Calculator {
 
       // create penel with the gridLayout that will contain buttons
         p1 = new JPanel(new GridLayout(4,3,2,2));
-        p1.add(button1);
-        p1.add(button2);
-        p1.add(button3);
-        p1.add(button4);
-        p1.add(button5);
-        p1.add(button6);
-        p1.add(button7);
-        p1.add(button8);
-        p1.add(button9);
-        p1.add(button0);
+        for (int index = 1; index <= buttons.length ; index++) {
+            p1.add(index==10?buttons[0]:buttons[index]);
+          //  index==10 ? p1.add(buttons[0]) : p1.add(buttons[index]);
+        }
+
         p1.add(buttonPoint);
         p1.add(buttonEqual);
 
